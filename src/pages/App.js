b1d8bce766f2114e75ec2869e57d0d25b5bfa151
Luis Santiago-Bustos <83996790+luissantiagobustos@users.noreply.github.com>
2023-02-24 13:07:12 -0600

    CHANGES: 1) ADDED libraries to app of quote history page 2) BUTTON added to return to previous page and style changes on button

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,7 @@
 import QuoteTable from "./QuoteTable"
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 
 export default function App() {
   
@@ -6,7 +9,14 @@ export default function App() {
     <nav className="main-body"> 
       <nav>
         <header className = "navigation-bar">
-            <Button variant="contained" /*onClick={}*/ > New Fuel Quote </Button>
+            <Stack direction="row" spacing={2} >
+                <Button variant="contained" /*onClick={}*/ >
+                  Previous Page
+                </Button>
+                <Button variant="contained"  /*onClick={}*/ color="success" endIcon={<SendIcon />}>
+                  New Fuel Quote
+                </Button>
+            </Stack>
         </header>
       </nav>
       <QuoteTable className />
