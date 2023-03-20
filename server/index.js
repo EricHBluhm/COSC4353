128431549d11dfda52cfa4b14abf14b5c529355c
Hunter Bluhm <ehb2727@gmail.com>
2023-03-19 22:15:10 -0500

    Creating auth route/controller
    Creating auth route/controller

--- server/index.js
+++ server/index.js
@@ -1,2 +1,15 @@
 //npm init -y
-//npm i express
+//npm i express, mongoose
+
+import express from "express"
+import authRoutes from "./routes/auth.js"
+
+const app = express();
+app.use(express.json());
+
+app.use("/server/auth", authRoutes);
+
+
+app.listen(8800, ()=>{
+    console.log("Port 8800")
+})
---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+import { Express } from "express";
+import { } from "../controllers/auth.js"
+
+const router = express.Router();
+
+router.get("")
+
+export default router
