0a5cb24850086d14b9e9c1aa9f4519f8b3016fe7
Hunter Bluhm <ehb2727@gmail.com>
2023-03-22 17:02:14 -0500

    test
    test

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 //npm init -y
-<<<<<<< HEAD
-//npm i express, mongoose, bycryptjs, jsonwebtoken
-//package.sjon - "type": "module", under main so we can
-    //do imports
+//npm i express, mongoose
 
 import express from "express"
 import authRoutes from "./routes/auth.js"
@@ -10,21 +7,9 @@ import authRoutes from "./routes/auth.js"
 const app = express();
 app.use(express.json());
 
-app.use("/server/auth", authRoutes); //on /server/auth, will use authRoutes
-//so in authRoutes, server/auth/something can be defined in server/auth
-//but only need to state /something in the authRoutes
-//authroutes then imports the function from the controller
+app.use("/server/auth", authRoutes);
 
 
 app.listen(8800, ()=>{
-    console.log("Connected Port 8800")
+    console.log("Port 8800")
 })
-
-
-
-// app.get("/test", (req,res)=>{
-//     //     res.json("It WORKS!")
-//     // })
-=======
-//npm i express
->>>>>>> parent of 1284315 (Creating auth route/controller)
