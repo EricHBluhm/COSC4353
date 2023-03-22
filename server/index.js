97ca64730652ebd3982599b6a8ff0cd91e92eb0e
Hunter Bluhm <ehb2727@gmail.com>
2023-03-22 17:02:39 -0500

    Revert "Creating auth route/controller"
    This reverts commit 128431549d11dfda52cfa4b14abf14b5c529355c.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,2 @@
 //npm init -y
-//npm i express, mongoose
-
-import express from "express"
-import authRoutes from "./routes/auth.js"
-
-const app = express();
-app.use(express.json());
-
-app.use("/server/auth", authRoutes);
-
-
-app.listen(8800, ()=>{
-    console.log("Port 8800")
-})
+//npm i express
--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import { Express } from "express";
-import { } from "../controllers/auth.js"
-
-const router = express.Router();
-
-router.get("")
-
-export default router
