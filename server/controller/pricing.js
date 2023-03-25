46ac2a2137ed23ba521e74ef45e76f227fa29ac8
Hunter Bluhm <ehb2727@gmail.com>
2023-03-24 20:23:37 -0500

    Created pricing class
    pricing class

---
+++ server/controller/pricing.js
@@ -0,0 +1,21 @@
+class Pricing {
+    constructor(gallonsRequested,address,date,sugPrice,due)
+    {
+        this.gallonsRequested = gallonsRequested,
+        this.address = address,
+        this.date = date,
+        this.sugPrice = sugPrice,
+        this.due = due
+    }
+
+    get Price(){
+        return this.calcPrice();
+    }
+
+    calcPrice() {
+       let amtDue = 100/this.gallonsRequested;
+       return amtDue; 
+    }
+
+}
+
