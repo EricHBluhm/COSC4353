94f60f8ac43a0a1df3ac28c185adb8a74fe245b6
RomerPropper <91396942+RomerPropper@users.noreply.github.com>
2023-03-24 23:04:38 -0500

    Update form.test.js

--- server/__test__/form.test.js
+++ server/__test__/form.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../server/app.js'
+import app from '../app.js'
 
 describe("Post to /quotes/quotform", () =>{
     describe("given form data", () =>{
