75c89dc3456e4aef330a3048726771261ddcc983
RomerPropper <91396942+RomerPropper@users.noreply.github.com>
2023-03-24 22:09:40 -0500

    Add files via upload

---
+++ server/schemas/quoteFormSchema.js
@@ -0,0 +1,12 @@
+import * as yup from "yup"
+
+let quoteInfoSchema = yup.object().shape({
+    gallonsRequested: yup.number().required().positive(),
+    address: yup.string().min(5).max(50).required(),
+    deliveryDate: yup.date().required(),
+    suggPrice: yup.number().required().positive(),
+    realPrice: yup.number().required().positive().default(100),
+});
+
+
+export default quoteInfoSchema
