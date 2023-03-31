fb85f2ea51099e1dc7e97b693684b87beaaa85f9
Hunter Bluhm <ehb2727@gmail.com>
2023-03-30 19:51:30 -0500

    added encryption helpers for unit testing
    Also commented out other test fiesl

--- server/__test__/backend-history.test.js
+++ server/__test__/backend-history.test.js
@@ -1,27 +1,27 @@
-import request from 'supertest'
-import app from '../app.js'
+// import request from 'supertest'
+// import app from '../app.js'
 
- describe("Post /history", () => {
+//  describe("Post /history", () => {
 
-    describe("test given json data", () => {
+//     describe("test given json data", () => {
 
-        test("should respond with a 200 status code", async () => {
-            const response = await request(app).post("/history").send({
-                ID: "ID"
-            })
-            expect(response.statusCode).toBe(200)
-        })
-        test("should specify json in the contenct type header", async () => {
-            const response = await request(app).post("/history").send({
-                ID: "ID"
-            })
-            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
-        })
+//         test("should respond with a 200 status code", async () => {
+//             const response = await request(app).post("/history").send({
+//                 ID: "ID"
+//             })
+//             expect(response.statusCode).toBe(200)
+//         })
+//         test("should specify json in the contenct type header", async () => {
+//             const response = await request(app).post("/history").send({
+//                 ID: "ID"
+//             })
+//             expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+//         })
 
-    })
+//     })
 
-    describe(" ", () =>{
+//     describe(" ", () =>{
 
-    })
+//     })
 
- })
+//  })
