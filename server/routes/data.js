b20018e77e9c6b391e8c401f463a61a5e62c1152
Luis Santiago-Bustos <83996790+luissantiagobustos@users.noreply.github.com>
2023-04-02 14:11:21 -0500

    Update data.js

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,20 +1,39 @@
 import express from "express";
 import data from "./placeHolder.json" assert { type: "json" };
+import db from "../app.js"
 const router = express.Router();
 
-router.route('/').get((req,res) => {
-    res.json(data)
+router.route('/:ID').get((req,res) => {
+    const { id } = req.params.ID
+    let quotes = []
+    
+    db.collection('quotes')
+        .find({ email: id }, { realPrice:1, suggPrice: 1, deliveryDate: 1, address: 1, gallonsRequested:1 })
+        .sort({ deliveryDate : 1})
+        .forEach(quote => quotes.push(quote))
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch(() => {
+            res.status(500).json({error: ''Could not fetch the documents'})
+        })
+        
+        // res.json({mssg: 'welcome to the api'})
 })
 
-// router.route('/').get((req,res) => {  
-//     res.sendStatus(200)
-// })
-
 router.route('/:ID').delete((req,res) => {
-    const { ID } = req.params;
-    delete data[ID]
-    // HERE database queries
+    const { id } = req.params.ID
     
+     db.collection('quotes')
+        .deleteOne({ email: id })
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch(() => {
+            res.status(500).json({error: ''Could not delete the documents'})
+        })
+        
+        // res.json({mssg: 'welcome to the api'})
 })
 
 export default router
