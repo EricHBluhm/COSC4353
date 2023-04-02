db3c484c1cd5ec5f4fdd2f1866b225a05f883b0b
Luis Santiago-Bustos <83996790+luissantiagobustos@users.noreply.github.com>
2023-04-02 18:29:26 -0500

    Update data.js

--- server/routes/data.js
+++ server/routes/data.js
@@ -8,24 +8,41 @@ router.route('/:ID').get( async (req,res) => {
     let { ID } = req.params
     ID = ID.substring(1)
     let quotes = []
-   console.log(ID)
-    quotes = await Quote.find({ email: ID }, { realPrice:1, suggPrice: 1, deliveryDate: 1, address: 1, gallonsRequested:1 })
+    console.log(ID)
 
-    // mongoose.connection.db.collection('quotes')
-    //     .find({ email: id }, { realPrice:1, suggPrice: 1, deliveryDate: 1, address: 1, gallonsRequested:1 })
-    //     .sort({ deliveryDate : 1})
-    //     .forEach(quote => quotes.push(quote))
-    //     .then(() => {
-    //         res.status(200).json(books)
-    //     })
-    //     .catch(() => {
-    //         res.status(500).json({error: 'Could not fetch the documents'})
-    //     })
+    const foundUser = await Quote
+        .findOne({ email: ID} ).exec(); //check to see if user exists
+
+    if(!foundUser) //if user doesn't exist
+    {
+            console.log("Not email for this quote request.")
+            return res.status(501).json('Could not fetch the documents.')
+    }
+    else{
+
+        quotes = await Quote
+            .find({ email: ID }, { realPrice:1, suggPrice: 1, deliveryDate: 1, address: 1, gallonsRequested:1 })
+            .sort({ deliveryDate : 1})
+            .then(() => {
+                res.status(201);
+            })
         
-        // res.json({mssg: 'welcome to the api'})
 
-    res.status(201);
-    return res.json(quotes); //return saved user to frontend
+        // mongoose.connection.db.collection('quotes')
+        //     .find({ email: id }, { realPrice:1, suggPrice: 1, deliveryDate: 1, address: 1, gallonsRequested:1 })
+        //     .sort({ deliveryDate : 1})
+        //     .forEach(quote => quotes.push(quote))
+        //     .then(() => {
+        //         res.status(200).json(books)
+        //     })
+        //     .catch(() => {
+        //         res.status(500).json({error: 'Could not fetch the documents'})
+        //     })
+            
+            // res.json({mssg: 'welcome to the api'})
+
+        return res.json(quotes); //return saved user to frontend
+    }
 })
 
 // router.route('/:ID').delete((req,res) => {
@@ -44,3 +61,4 @@ router.route('/:ID').get( async (req,res) => {
 // })
 
 export default router
+
