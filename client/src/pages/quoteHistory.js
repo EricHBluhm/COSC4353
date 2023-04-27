a59178d1150d4a86d07a07fee5c993984877fc00
Luis Santiago-Bustos <83996790+luissantiagobustos@users.noreply.github.com>
2023-04-26 21:47:42 -0500

    Update quoteHistory.js

--- client/src/pages/quoteHistory.js
+++ client/src/pages/quoteHistory.js
@@ -1,6 +1,9 @@
 import QuoteTable from "./QuoteTable"
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
+import Divider from '@mui/material/Divider';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import Stack from '@mui/material/Stack';
 import { useNavigate } from "react-router-dom";
 import "./quoteTable.css";
@@ -10,24 +13,37 @@ export default function QuoteHistory() {
   const navigate = useNavigate()
 
   return (
-    <nav className="main-body"> 
+    <div className="main-body"> 
       <nav>
         <header className = "navigation-bar">
-            <Stack direction="row" spacing={2} >
-                <Button variant="contained" onClick={() => { navigate("/Login"); }} >
+            <Stack     direction= "row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={5}
+            justifyContent="space-between"
+            alignItems="center"
+            >
+                <Button variant="contained" onClick={() => { navigate("/Login"); }} endIcon={<ExitToAppIcon />}>
                   Log out
                 </Button>
-                <Button variant="contained"  onClick={() => { navigate("/QuoteForm"); }} color="success" endIcon={<SendIcon />}>
+                <Button variant="contained"  onClick={() => { navigate("/Register"); }} color="warning" startIcon={<AccountBoxIcon />}>
+                  Registration Settings
+                </Button>
+                <Button variant="contained"  onClick={() => { navigate("/QuoteForm"); }} color="success" endIcon={<RequestQuoteIcon />}>
                   New Fuel Quote
                 </Button>
             </Stack>
         </header>
       </nav>
-      <QuoteTable className />
-      <footer className = "footer-main"> 
-          <center><small>© 2023 All Rights Reserved. University of Houston. Department of Computer Science.</small></center>
-      </footer>
-    </nav>
+      <div className="wrapper">
+        <div className="table">
+          <QuoteTable className />
+        </div>
+        <footer className = "footer-main"> 
+            <center><small>© 2023 All Rights Reserved. University of Houston. Department of Computer Science.</small></center>
+        </footer>
+      </div>
+    </div>
   )
 }
+
 
